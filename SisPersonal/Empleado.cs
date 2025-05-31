01fb430615fc9deef0190cfc6976f0a06dd7805a
Gonz0x <gonzafigueroa6400@gmail.com>
2025-05-31 16:07:22 -0300

    ultimo commit

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,37 +1,91 @@
+namespace SisPersonal
+{
+    public enum Cargos{Auxiliar, Administrativo, Ingeniero, Especialista, Investigador};
+    public class Empleado{
+        private string nombre;
+        private string apellido;
+        private DateTime fechadenac;
+        private char estadoCivil;
+        private DateTime ingresoEmpresa;
+        private double sueldo; 
+        private Cargos cargo;
 
-public class Empleado{
-    private string nombre;
-    private string apellido;
-    private DateTime fechadenac;
-    private char estadoCivil;
-    private DateTime ingresoEmpresa;
-    private double sueldo; 
+        
+        public string Nombre {get => nombre; set => nombre = value;}
+        public string Apellido {get => apellido; set => apellido = value;}
+        public DateTime Fechanac {get => fechadenac; set => fechadenac = value;}
+        public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+        public DateTime IngresoEmpresa { get => ingresoEmpresa; set => ingresoEmpresa = value; }
+        public double Sueldo { get => sueldo; set => sueldo = value;}
+        public Cargos Cargo {get => cargo; set => cargo = value;}
 
-    private enum Cargos{Auxiliar, Administrativo, Ingeniero, Especialista, Investigador};
-    
-    public string Nombre {get => nombre; set => nombre = value;}
-    public string Apellido {get => apellido; set => apellido = value;}
-    public DateTime Fechanac {get => fechadenac; set => fechadenac = value;}
-    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
-    public DateTime IngresoEmpresa { get => ingresoEmpresa; set => ingresoEmpresa = value; }
-    public double Sueldo { get => sueldo; set => sueldo = value; }
+        public int GetEdad(){
+            return System.DateTime.Now.Year - fechadenac.Year;
+        }
+        public int GetAntiguedad(){
+            return System.DateTime.Now.Year - ingresoEmpresa.Year;
+        }
+        public Empleado(string nombre, string apellido, DateTime fechadenac, char estadoCivil, DateTime ingresoEmpresa, double sueldo, Cargos cargo){
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechadenac = fechadenac;
+            this.estadoCivil = estadoCivil;
+            this.ingresoEmpresa = ingresoEmpresa;
+            this.sueldo = sueldo;
+            this.cargo = cargo;
+        }
+        public string Jubilacion()
+        {
+            int edad = GetEdad();                
+            int añosFaltantes = 65 - edad;
+            if(edad < 65){
+                return($"falta para jubilarse:{añosFaltantes}");
+            }else{
+                return("cumple los años para la jubilacion.");
+            }
+        }
 
-    public int GetEdad(){
-        return fechadenac.Year - System.DateTime.Now.Year;
+        public int paraJubilarse(){
+            int edad = GetEdad();
+            int añosFaltantes = 65 - edad;
+            return añosFaltantes;
+        }
+
+        public double salario(){
+            double salario;
+            double adicional;
+            int antiguedad = GetAntiguedad();
+            if(antiguedad <= 20){
+                adicional = sueldo * antiguedad * 0.01;
+            }else{
+                adicional = sueldo * 0.25;
+            }
+            if(Cargo == Cargos.Ingeniero || Cargo == Cargos.Especialista){
+                adicional *= 1.50;
+            }
+
+            if(EstadoCivil == 'C'){
+                adicional += 150000;
+            }
+
+            salario = sueldo + adicional;
+            return salario;
+        }
+        public string MostrarEmpleado()
+        {
+            return $"Nombre: {nombre} {apellido}\n" + 
+            $"Fecha de Nacimiento: {fechadenac} (Edad: {GetEdad()})\n" +
+            $"Estado Civil: {estadoCivil}\n" +
+            $"Fecha de Ingreso: {ingresoEmpresa} (Antigüedad: {GetAntiguedad()} años)\n" +
+            $"Sueldo: {sueldo}\n" +
+            $"Cargo: {cargo}\n" +
+            $"Jubilación: {Jubilacion()}\n" +
+           $"Salario Total: {salario()}";
+        }
+
+
+
     }
-    public int GetAntiguedad(){
-        return ingresoEmpresa.Month - System.DateTime.Now.Month;
-    }
-    public Empleado(string nombre, string apellido, DateTime fechadenac, char estadoCivil, DateTime ingresoEmpresa, double sueldo){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.fechadenac = fechadenac;
-        this.estadoCivil = estadoCivil;
-        this.ingresoEmpresa = ingresoEmpresa;
-        this.sueldo = sueldo;
-    }
-    public string mostrarEmpleado(){
-        return "nombre:"+ nombre + "apellido:"+ apellido + "fecha nac:" + fechadenac + "estado civil:" +estadoCivil + "Fecha ingreso:"+ingresoEmpresa + "sueldo" +sueldo;
-    }
+
 
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SisPersonal;
+Empleado jose = new Empleado("Jose", "Figueredo", new DateTime(2000, 10, 12), 'C', new DateTime(2018, 10, 11), 900000.50, Cargos.Ingeniero);
+Console.WriteLine("Datos del Empleado:");
+Console.WriteLine(jose.MostrarEmpleado());
 
-Empleado jose = new Empleado("Jose", "Figueredo", new DateTime(2000, 10, 12), 'C', new DateTime(2018, 10, 11), 900000);
+Empleado [] empleados = new Empleado[3];
+empleados[0] = new Empleado("Jamar", "Pines", new DateTime(2001, 11, 3), 'C', new DateTime(2019, 9, 3), 160000, Cargos.Ingeniero);
+empleados[1] = new Empleado("Franklin", "James", new DateTime(1980, 7, 8), 'S', new DateTime(2009, 3, 2), 150000, Cargos.Especialista);
+empleados[2] = new Empleado("Jennifer", "Aniston", new DateTime(2003, 7, 8), 'C', new DateTime(2020, 7, 9), 130000, Cargos.Administrativo);
+
+foreach (var emp in empleados)
+{
+    Console.WriteLine($"Empleado: {emp.Nombre}");
+    Console.WriteLine($"Antigüedad: {emp.GetAntiguedad()} años");
+    Console.WriteLine($"Salario total: {emp.salario()}");
+    Console.WriteLine("--------------------------");
+}
+
+double montoTotal = 0;
+foreach(var emp in empleados)
+{
+    montoTotal += emp.salario();
+}
+Console.WriteLine($"Monto total en salarios: {montoTotal:C}");
+
+/* int menor = 100;
+foreach(var emp in empleados){
+    int añosParaJubilar = emp.paraJubilarse();
+    if(añosParaJubilar < menor){
+        menor = añosParaJubilar;
+        proximo = emp;
+    }
+}
+Console.WriteLine("Empleado más próximo a jubilarse:");
+Console.WriteLine(menor.MostrarEmpleado());
+Console.WriteLine($"Años para jubilarse: {menor}"); */
